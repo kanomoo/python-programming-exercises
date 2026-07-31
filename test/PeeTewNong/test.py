b01abf1db0fc38af98670fb8ddf84e2f5ba5fd9e
few717254 <few717254@gmail.com>
2026-07-31 20:11:02 +0700

    feat: add program to find maximum value from user input with enhanced validation

--- test/PeeTewNong/test.py
+++ test/PeeTewNong/test.py
@@ -10,19 +10,19 @@
 #     print(f"Maximum Digit of integer number {num} = {Max}")
 
 # code ที่ไม่มีการ comment ใช้เฉพาะ while ทำไมไม่รู้
-print(">> Program Find Maximum Digit <<")
-while True:
-    Max = 0
-    number = int(input("Enter integer number(0-exit) : "))
-    num = number
-    if number == 0:
-        print("Exit Program")
-        break
-    while num > 0:
-        digit = num % 10
-        num //= 10
-        if digit > Max: Max = digit
-    print(f"Maximum Digit of integer number {number} = {Max}")
+# print(">> Program Find Maximum Digit <<")
+# while True:
+#     Max = 0
+#     number = int(input("Enter integer number(0-exit) : "))
+#     num = number
+#     if number == 0:
+#         print("Exit Program")
+#         break
+#     while num > 0:
+#         digit = num % 10
+#         num //= 10
+#         if digit > Max: Max = digit
+#     print(f"Maximum Digit of integer number {number} = {Max}")
 
 
 # print(">> Program Find Maximum Digit <<")
@@ -38,3 +38,24 @@ while True:
 #         num //= 10 # num //= 10 หรือ num = num // 10 คือการลดค่าทีละหลักของ num โดยใช้ // 10 คือการหาร 10 และตัดเศษทิ้ง หรือการตัดค่าหลักหน่วยออกไป
 #         if digit > Max: Max = digit # if เช็ค5hk digit มากกว่าตัวแปร Max ให้ digit ใน loop นั้นเป็นตัวแปร Max แทน หรือ ค่าสูงสุด
 #     print(f"Maximum Digit of integer number {number} = {Max}") # โชว์การแสดงผม
+
+
+print(">> Program Find Maximum Value <<")
+while True:
+    output = "Your enter number : "
+    numVal = int(input("Enter number of value(>= 1) : "))
+    Max = 0
+    if numVal <= 1:
+        print("Value input not correct.\nExit Program")
+        break
+    print(f"\nProgram get value {numVal} numbers.")
+    for i in range(1, numVal + 1):
+        num = int(input(f"Enter value Number #{i} :"))
+        if num > Max: Max = num
+        output += f"{num}, " if i < numVal else f"{num}"
+    print(output)
+    print(f"Maximum value number is {Max}")
+    print("Exit Program")
+    break
+    
+
